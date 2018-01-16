cd1df7b3b1cebe7905f5ce9c1dbd80130720d684
Vinayak <vinayakkaushikk@gmail.com>
2018-01-16 09:39:46 +0530

    Juggling Async done

--- juggling_async.js
+++ juggling_async.js
@@ -1,28 +1,26 @@
 var http = require('http')
 var async = require('async')
 
+var papyrus = new Array(3)
+var counter = 0
 
-if (process.argv.length > 2) {
-	for ( var i = 2; i < process.argv.length; i++) {
-		http.get(process.argv[i], function (res) {
+for ( let i = 2; i < process.argv.length; i++) {
+	http.get(process.argv[i], function (res) {
+		let text = ""
+		res.setEncoding('utf8')
+		res.on('data', function (data) {
+			text += data
+		})
+		res.on('end', function () {
 			counter += 1
-			text = ""
-			res.setEncoding('utf8')
-			res.on('data', function (data) {
-				text += data
-			})
-			res.on('end', function () {
-				papyrus[i] = text
-				counter -= 1
-			})
-			res.on('error', console.error)
-		}).on('error', console.error);
-	}
+			papyrus[i-2] = text
+			if (counter == 3) {
+				for ( var b = 0; b < 3; b++) {
+					console.log(papyrus[b])
+				}
+			}
+		})
+		res.on('error', console.error)
+		
+	}).on('error', console.error);
 }
-
-var urls = process.argv.slice(2,);
-
-async.eachSeries(urls, function(data, callback) {
-
-
-})
